9deaed720f1d63fede238910f46a882c333c475c
Aditya_Nikhate <nikhateaditya123@gmail.com>
2024-06-15 19:29:22 +0530

    Some update

--- frontend/src/components/AI.jsx
+++ frontend/src/components/AI.jsx
@@ -4,11 +4,11 @@ const AI = () => {
   const [answer, setAnswer] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [text, setText] = useState("it was great session lot to learn from you. is it gramatically correct rewrite it");
+  const [text, setText] = useState();
   const apiKey = "Api__key"
   const fetchData = async (text) => {
     setLoading(true);
-    setError(null);
+    setError(null);  
     try {
       const response = await axios.post('/api/gemini', { text }, {
         headers: { 'Authorization': `Bearer ${apiKey}` },
@@ -31,7 +31,14 @@ const AI = () => {
       <div className='mt-20 m-10 '>
         <h1 className='text-xl font-bold uppercase text-center'>AI BOT in service</h1>
         <textarea onChange={(e)=>(setText(e.target.value))} value={text} rows={3} placeholder='write the content of the post.....' className='w-full bg-blue-100 mt-5 p-2 rounded border-2 border-gray-500'></textarea>
+        <button className='w-full py-2 text-center mt-4 rounded-full text-xl font-bold text-white bg-[#1EA1F2]'>Submit</button>
       </div>
+
+      {
+        answer&&(<div>
+        <p className='w-full p-10'>{answer}</p>
+        </div>)
+      }
     </div>
   )
 }
